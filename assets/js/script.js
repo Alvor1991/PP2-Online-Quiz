c7f693e2dbbe0c74b0379ff19c462b77b2f41c0c
Ryan Nolan <ryannolan_@hotmail.com>
2024-02-13 15:20:41 +0000

    add first question in js

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const quiz = [
+    {
+        question: "Practice Question?",
+        answers: [
+            { text: "Answer1", correct: false},
+            { text: "Answer2", correct: false},
+            { text: "Answer3", correct: true},
+            { text: "Answer4", correct: false},
+        ]
+    }
+]
