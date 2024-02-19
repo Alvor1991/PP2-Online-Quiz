4739b9b0dba07158d7f1731980d4857f962ee9ad
Ryan Nolan <ryannolan_@hotmail.com>
2024-02-19 17:29:21 +0000

    Add another question to test progress bar changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,7 +37,18 @@ const quiz = [
             { text: "Answer3", correct: false},
             { text: "Answer4", correct: false},
         ]
+    },
+
+    {
+        question: "Practice Question?",
+        answers: [
+            { text: "Answer1", correct: true},
+            { text: "Answer2", correct: false},
+            { text: "Answer3", correct: false},
+            { text: "Answer4", correct: false},
+        ]
     }
+
 ];
 
 
